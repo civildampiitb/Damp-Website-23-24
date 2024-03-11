e4ed0c5c3af9f700eef1fa724102d5059b329691
civildampiitb <ishitgarg1231@gmail.com>
2024-03-12 01:39:38 +0530

    FAQS Completed

--- src/data/Faaqsdata.js
+++ src/data/Faaqsdata.js
@@ -18,54 +18,51 @@ const faqsData = [
     answer:
       "All Civil Engineering courses (CEXXX) can be tagged as Dept. Electives, except for Core Courses. You can view these at asc.iitb.ac.in under Academic --> All About Courses --> Running Courses and register for the upcoming semester accordingly",
   },
-  // {
-  //   id: 4,
-  //   question: "Can I modify it?",
-  //   answer:
-  //     "Yes, you are free to modify and use Lorem Ipsum for your own purposes.",
-  // },
-  // {
-  //   id: 5,
-  //   question: "Where can I get it?",
-  //   answer:
-  //     "You can generate Lorem Ipsum at various websites that offer Lorem Ipsum generators.",
-  // },
-  // {
-  //   id: 6,
-  //   question: "How long has Lorem Ipsum been used in the industry?",
-  //   answer:
-  //     "Lorem Ipsum has been the industry's standard dummy text since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  // },
-  // {
-  //   id: 7,
-  //   question: "Why is it called Lorem Ipsum?",
-  //   answer:
-  //     "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
-  // },
-  // {
-  //   id: 8,
-  //   question: "Can I use Lorem Ipsum for commercial purposes?",
-  //   answer:
-  //     "Yes, you can use Lorem Ipsum for commercial purposes. It is free to use for any purpose, including commercial use.",
-  // },
-  // {
-  //   id: 11,
-  //   question: "Can I use Lorem Ipsum for commercial purposes?",
-  //   answer:
-  //     "Yes, you can use Lorem Ipsum for commercial purposes. It is free to use for any purpose, including commercial use.",
-  // },
-  // {
-  //   id: 9,
-  //   question: "Can I use Lorem Ipsum for commercial purposes?",
-  //   answer:
-  //     "Yes, you can use Lorem Ipsum for commercial purposes. It is free to use for any purpose, including commercial use.",
-  // },
-  // {
-  //   id: 10,
-  //   question: "Can I use Lorem Ipsum for commercial purposes?",
-  //   answer:
-  //     "Yes, you can use Lorem Ipsum for commercial purposes. It is free to use for any purpose, including commercial use.",
-  // },
+  {
+    id: 4,
+    question: "What is a minor? How would pursuing a minor help me?",
+    answer:
+      "Minor is an additional credential a student will earn if he/she does minimum 30 credits worth of additional learning in a discipline other than his/her major discipline of B.Tech/DD/etc. A minor degree provides an opportunity for students to enhance their academic learning experience.",
+  },
+  {
+    id: 5,
+    question: "Who is eligible to pursue a minor?",
+    answer:
+      "Any B.Tech Student with Academic Standing – Category I, II and III is eligible to pursue a minor.",
+  },
+  {
+    id: 6,
+    question: "How is the allotment done?",
+    answer:
+      "The allotment of minors (due to limited seats) is done on the basis of Core CPI after taking students’ preferences. However, waitlisting of students is done on first come first serve basis.",
+  },
+  {
+    id: 7,
+    question:
+      "If I get an FR Grade in a minor course do I need to repeat the course? And will it reflect in my final transcript?",
+    answer:
+      "No, you do not need to repeat the course. However, an ‘FR’ grade will reflect in the course will reflect under the additional courses done.",
+  },
+  {
+    id: 8,
+    question: "Does the Minor grade affect my SPI/CPI?",
+    answer:
+      "No. When you complete your minor, a Minor CPI will reflect on your transcript. Before that, the courses will be counted as additional learning.",
+  },
+  {
+    id: 9,
+    question:
+      "What are the other types of tags and how are they different from each other?",
+    answer:
+      "Institute Elective – Courses other than that from your dept. Dept. Elective –  Courses from your dept. (a list is there on civil website in your curriculum for the same). Additional Learning Course – Courses from any dept. (including yours) Not counted in Core CPI Audit – Courses from any dept. (including yours); Not counted in Core CPI (non-credit courses with only AU and NP grades) Sit-Through – Courses from any dept. for which you do not register but attend classes due to interest with the permission of the instructor.",
+  },
+  {
+    id: 10,
+    question:
+      "Is it compulsory to complete the various electives in the order specified in the curriculum?",
+    answer:
+      "No, the order specified in the curriculum is the recommended order to complete the courses so that you do not have to overload yourself with a lot of courses in subsequent semesters. But, you can choose to complete the electives in whichever order you wish.",
+  },
 ];
 
 export default faqsData;
